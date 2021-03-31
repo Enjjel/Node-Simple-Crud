c08f46aeb936b4a98c48bb0db7d608ff9d52f08b
Enjjel <asmcardozoo@gmail.com>
2021-03-31 11:01:46 -0300

    adding env keys

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,10 +2,9 @@ const { Router } = require('express')
 const router = Router()
 const controllerUser = require('../controllers/user.controller')
 
-const {validateFields,
-     validateJWT,
-      validRoles,
-       isAdminRole} = require ('../middlewares/index')
+const validateFields  = require('../middlewares/validateFields')
+const validateJWT = require('../middlewares/validateJWT')
+const {validRole, isAdminRole} = require('../middlewares/validateRoles')
 const {roleValidator, emailValidator, existsUserById} = require('../helpers/dbValidations')
 const { check } = require('express-validator')
 
@@ -17,7 +16,7 @@ router.post('/new-user', [
     check('password', 'Password is required and has to be more than six letters').not().isEmpty().isLength({min: 6}),
     check('email','invalid email').isEmail(),
     check('email').custom(emailValidator),
-    check('role').custom(roleValidator),//role => roleValidator(role))
+    check('role').custom(roleValidator),
     validateFields
 ], controllerUser.postUsers)
 
